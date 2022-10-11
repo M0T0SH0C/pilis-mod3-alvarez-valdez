23faca85bb3912e2a0632bdc204fe98543bb652d
Alvaro Alvarez <tecnoasys@gmail.com>
2022-10-11 16:09:47 -0300

    3

--- src/components/Formularios.jsx
+++ src/components/Formularios.jsx
@@ -1,17 +1,41 @@
-import React, { useEffect, useInsertionEffect } from 'react'
+import React, { useEffect,} from 'react'
 import "../components/Formularios.css";
-import { CrearUbi } from './funciones/Funciones';
+import { CrearUbi } from '../components/funciones/Funciones';
 
 
 const Formularios = () => {
 
-useEffect(()=>{CrearUbi()},[])
+useEffect (()=>{CrearUbi()},[]);
 
-  return (    
-    <div>
-      <h1>hola</h1>
-      </div>
+  return (     
+    <form className='Latitud'>
+      <p>INGRESE NOMBRE DEL LUGAR </p>
+      <input type ='text' name= 'Nombre' autoCompletar = 'on' ></input>
+        
+      <br></br>
+      <br></br>
+      <br></br>
+
+     <p>INGRESE LATITUD</p>
+     <input type ='number' name= 'Latitud' autoCompletar = 'on' ></input>
+     
+     <br></br>
+     <br></br>
+     <br></br>
+      <p>INGRESE LONGITUD</p>
+     <input type ='number' name= 'Longitud' autoCompletar = 'on' ></input>    
+     <br></br>
+     <br></br>
+     <br></br>
+     
+     
+     <button type='submit'>CREAR</button>
+
+
+
+   </form>   
+
   )
-}
+};
 
 export default Formularios
