22f74989d3588735ec681e001d50452408be3ce8
Alvaro Alvarez <tecnoasys@gmail.com>
2022-10-14 05:09:07 -0300

    casi terminado

--- src/routes/Mapsygeo/Mapsygeos.jsx
+++ src/routes/Mapsygeo/Mapsygeos.jsx
@@ -3,6 +3,8 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import MarkerIcon from "../../assets/punto.png";
+import { useEffect, useRef } from "react";
+import "./Mapsygeos.css";
 
 const icon = L.icon({
   iconUrl: MarkerIcon,
@@ -16,7 +18,7 @@ const MapasOnline = () => {
       center={position}
       zoom={13}
       scrollWheelZoom={false}
-      style={{ width: "100vw", height: "100vw" }}
+      style={{ width: "100vw", height: "50vw" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -26,4 +28,32 @@ const MapasOnline = () => {
     </MapContainer>
   );
 };
+
+// Te dejo codigo de un relojo por si queres agregarlo //
+
+/*function reloj() {
+  const h1 = useRef();
+  const ti = () => {
+    const fechahora = new Date();
+    const hora = fechahora.getHours();
+    const minuto = fechahora.getMinutes();
+    const segundo = fechahora.getSeconds();
+    return `${hora}:${minuto}:${segundo}`;
+  };
+  useEffect(() => {
+    const cl = setInterval(() => {
+      h1.current.innerHTML = `${ti()}`;
+    }, 1000);
+    console.log("asd");
+    return () => clearInterval(cl);
+  }, []);
+  console.log("asdsss");
+  return (
+    <div className="reloj">
+      <h1 ref={h1}>{ti()}</h1>
+      <h2>HORA ACTUAL DE TU PROVINCIA</h2>
+    </div>
+  );
+}*/
+
 export default MapasOnline;
