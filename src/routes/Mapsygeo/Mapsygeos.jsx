682287e31b73eb4f62207b963d92d2d8df2ad390
Alvaro Alvarez <tecnoasys@gmail.com>
2022-10-14 04:22:15 -0300

    3

--- src/routes/Mapsygeo/Mapsygeos.jsx
+++ src/routes/Mapsygeo/Mapsygeos.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import MarkerIcon from "../../assets/punto.png";
@@ -16,14 +16,13 @@ const MapasOnline = () => {
       center={position}
       zoom={13}
       scrollWheelZoom={false}
-      style={{ width: "80vw", height: "80vw" }}
+      style={{ width: "100vw", height: "100vw" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={position} icon={icon}></Marker>
-      <Popup>Tu ubicacion</Popup>
     </MapContainer>
   );
 };
